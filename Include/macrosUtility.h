465f5a4f4fa22414f462473b3513e8e61073937e
blackbird <blackbirdcry806@gmail.com>
2020-01-28 23:56:24 +0100

    Added stringify

--- Include/macrosUtility.h
+++ Include/macrosUtility.h
@@ -3,3 +3,6 @@
 #define CONCATENATE_DETAIL(x, y) x##y
 #define CONCATENATE(x, y) CONCATENATE_DETAIL(x, y)
 #define MAKE_UNIQUE_NAME(x) CONCATENATE(x, __COUNTER__)
+
+#define STRINGIFY_DETAIL(x) #x
+#define STRINGIFY(x) STRINGIFY_DETAIL(x)
